5c51b4fac59f02b718072682b7fecc5ed49d0685
GUIMER SENON COAQUIRA COAQUIRA <guicoaquirac@upt.pe>
2020-06-11 21:10:54 -0500

    Conexion de la base de datos

---
+++ Proyecto-Thakhi-Web-Admin/Config/global.php
@@ -0,0 +1,20 @@
+<?php
+//IP de la pc servidor de la base de datos
+define("DB_HOST","localhost");
+
+//Nombre de la base de datos
+define("DB_NAME", "administracionthakhi");
+
+//Usuario de la base de datos
+define ("DB_USERNAME","root");
+
+//Contraseña del usuario de la base de datos
+define("DB_PASSWORD", "");
+
+//definimos la codificacion de los caracteres
+define("DB_ENCODE","utf8");
+
+//Definicmos una constante como nombre del proyecto
+define("PRO_NOMBRE","Thakhi - Delivery")
+
+?>
