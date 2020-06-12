7e101ddc29fc0b9f53d5b5006b8147511839fca4
GUIMER SENON COAQUIRA COAQUIRA <guicoaquirac@upt.pe>
2020-06-11 20:50:43 -0500

    Actulizacion de codigo

--- Proyecto-Thakhi-Web-Admin/Views/Template/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="footer text-center text-sm-left">
-    &copy; 2019 Thakhi Delivery <span class="text-muted d-none d-sm-inline-block float-right">Construcción de Softwre I</span>
-</footer><!--end footer-->
