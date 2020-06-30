6ee5ec18791b35492e44c2a98d48b58629486b41
J-Alfaro <J-Alfaro>
2020-06-29 20:04:13 -0500

    Modelo Obtener Cliente
    Modelo Obtener Cliente

--- Proyecto-Thakhi-Web-Admin/app/models/Cliente.php
+++ Proyecto-Thakhi-Web-Admin/app/models/Cliente.php
@@ -25,6 +25,31 @@ class   Cliente{
 
         return $result;
     }
+    //Modelo Obtener Cliente
+    public function obtener(int $id) : Cliente{
+        $result = new Cliente;
+
+        try {
+            $stm = $this->pdo->prepare('select * from admclitcliente where CLIdni = ?');
+            $stm->execute([$id]);
+
+            $fetch = $stm->fetch();
+
+            $result->CLIdni = $fetch->CLIdni;
+            $result->CLInombre = $fetch->CLInombre;
+            $result->CLIapellido = $fetch->CLIapellido;
+            $result->CLIcelular = $fetch->CLIcelular;
+            $result->CLIemail = $fetch->CLIemail;
+            $result->CLIclave = $fetch->CLIclave;
+
+            
+           
+        } catch(Exception $e) {
+
+        }
+
+        return $result;
+    }
 
 
 
