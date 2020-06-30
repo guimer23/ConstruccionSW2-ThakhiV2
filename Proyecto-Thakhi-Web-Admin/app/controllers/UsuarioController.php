14fdaa3f5c95e53520e8686988a057195d8a58a1
guimer23 <41280675+guimer23@users.noreply.github.com>
2020-06-29 20:01:22 -0500

    Metodo guardar UsuarioController

--- Proyecto-Thakhi-Web-Admin/app/controllers/UsuarioController.php
+++ Proyecto-Thakhi-Web-Admin/app/controllers/UsuarioController.php
@@ -32,6 +32,38 @@ class   UsuarioController{
         require_once _VIEW_PATH_ . 'footer.php';
     }
 
+    //Metodo guardar UsuarioController
+    public function guardar() { 
+
+        $nombreImg=$_FILES['imagen']['name'];
+        $rutaAlmacenamiento=$_FILES['imagen']['tmp_name'];
+        $carpeta='public/images/';
+        $rutaFinal=$carpeta.$nombreImg;
+
+        if(!empty($nombreImg)){
+            move_uploaded_file($rutaAlmacenamiento, $rutaFinal);
+        }
+       
+        $model = new \App\Models\Usuario;
+        $model->USUid = $_POST['USUid'];
+        $model->USUnombre = $_POST['USUnombre'];
+        $model->USUapellidos = $_POST['USUapellidos'];
+        $model->USUemail = $_POST['USUemail'];
+        $model->USUusuario = $_POST['USUusuario'];
+        $model->USUpassword = $_POST['USUpassword'];
+        $model->USUestado = $_POST['USUestado'];
+        $model->ruta_foto = $rutaFinal;
+
+        $result = $this->usuario->guardar($model);
+
+        if(!$result) {
+            throw new Exception('No se pudo realizar la operación');
+        } else {            
+            header('location: ?c=usuario');
+        }
+    }
+
+
 }
 
 ?>
