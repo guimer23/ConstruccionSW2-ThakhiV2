2a39a9341a4fb63bccabd7b49829c50249096e9a
J-Alfaro <J-Alfaro>
2020-06-29 19:54:06 -0500

    Controlador Index Cliente
    Controlador Index Cliente

--- Proyecto-Thakhi-Web-Admin/app/controllers/ClienteController.php
+++ Proyecto-Thakhi-Web-Admin/app/controllers/ClienteController.php
@@ -4,8 +4,20 @@ namespace App\Controllers;
 class   ClienteController{
 
     private $cliente;
+      public function __construct(){
+        $this->cliente = new \App\Models\Cliente;
+      
+    }
+    //Controlador Index Cliente
+    public function index() {
+        $model = $this->cliente->listar();
+        //  $model = $this->empleado->listar();
+        require_once _VIEW_PATH_ . 'header.php';
+        require_once _VIEW_PATH_ .'cliente/index.php';
+        require_once _VIEW_PATH_ . 'footer.php';
+    }
 
-    
+
 }
 
 ?>
