91a360b1037cd363949664eb801a33745b693a72
J-Alfaro <J-Alfaro>
2020-06-29 20:03:24 -0500

    Modelo Listar Cliente
    Modelo Listar Cliente

--- Proyecto-Thakhi-Web-Admin/app/models/Cliente.php
+++ Proyecto-Thakhi-Web-Admin/app/models/Cliente.php
@@ -4,7 +4,27 @@ use Exception;
 class   Cliente{
 
     private $pdo;
-    
+        public function __construct(){
+        $this->pdo=\Core\Database::getConnection();
+
+    }
+
+    //Modelo Listar Cliente
+    public function listar() : array{
+        $result = [];
+
+        try {
+            $stm = $this->pdo->prepare('SELECT  * from admclitcliente');
+            $stm->execute();
+
+            $result = $stm->fetchAll();
+        } catch(Exception $e) {
+            echo 'ERROR!';
+            print_r( $e );
+        }
+
+        return $result;
+    }
 
 
 
