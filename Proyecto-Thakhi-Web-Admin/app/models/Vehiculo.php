ff02996a5bcb859dfd9a57a021776571f5c25193
JhonPeterAguilarAtencio <42424870+JhonPeterAguilarAtencio@users.noreply.github.com>
2020-07-01 20:27:35 -0500

    Creacion del modelo Vehiculo
    Creacion del modelo Vehiculo
    creacion de la funcion Listar()

---
+++ Proyecto-Thakhi-Web-Admin/app/models/Vehiculo.php
@@ -0,0 +1,31 @@
+<?php   
+namespace App\Models;
+use Exception;
+class   Vehiculo{
+
+    private $pdo;
+
+    public function __construct(){
+        $this->pdo=\Core\Database::getConnection();
+
+    }
+
+    public function listar() : array{
+        $result = [];      
+        try {
+            $stm = $this->pdo->prepare('SELECT  * from admvehtvehiculo');
+            $stm->execute();
+            $result = $stm->fetchAll();
+
+        } catch(Exception $e) {
+            echo 'ERROR!';
+            print_r( $e );
+        }
+
+        return $result;
+    }
+
+}
+
+
+?>
