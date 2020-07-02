11c1ef04243e53ea93f2e8cf9537c01d66700d2b
JhonPeterAguilarAtencio <42424870+JhonPeterAguilarAtencio@users.noreply.github.com>
2020-07-01 20:35:49 -0500

    Creacion de la funcion guardar()
    creacion de la funcion guardar dentro del modelo Vehiculo

--- Proyecto-Thakhi-Web-Admin/app/models/Vehiculo.php
+++ Proyecto-Thakhi-Web-Admin/app/models/Vehiculo.php
@@ -49,6 +49,69 @@ class   Vehiculo{
         return $result;
     }
 
+    public function guardar(Vehiculo $model) : bool{
+        $result = false;
+
+        try {
+
+            if(empty($model->VEHid)){
+                        $sql = '
+                    insert into admvehtvehiculo(
+                        VEHplaca,
+                        VEHmarca,
+                        VEHmodelo,
+                        VEHcolor,
+                        VEHanio_fabricacion,
+                        VEHestado,
+                        ruta_foto
+                    ) values (?,?,?,?,?,?,?)';
+
+                $stm = $this->pdo->prepare($sql);
+                $stm->execute([
+                    $model->VEHplaca,
+                    $model->VEHmarca,
+                    $model->VEHmodelo,
+                    $model->VEHcolor,
+                    $model->VEHanio_fabricacion,
+                    $model->VEHestado,
+                    $model->ruta_foto
+                ]);  
+//VEHsoat
+            } else {
+                $sql = '
+                    update admvehtvehiculo
+                    set 
+                    VEHplaca = ?,
+                    VEHmarca = ?,
+                    VEHmodelo = ?,
+                    VEHcolor = ?,
+                    VEHanio_fabricacion = ?,
+                    VEHsoat = ?,
+                    VEHestado = ?
+                    where VEHid = ?
+                ';
+
+                $stm = $this->pdo->prepare($sql);
+                $stm->execute([
+                    $model->VEHplaca,
+                    $model->VEHmarca,
+                    $model->VEHmodelo,
+                    $model->VEHcolor,
+                    $model->VEHanio_fabricacion,
+                    $model->VEHsoat,
+                    $model->VEHestado,
+                    $model->VEHid
+                ]);
+            }
+
+            $result = true;
+        } catch(Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+
+        return $result;
+    }
+
 }
 
 
