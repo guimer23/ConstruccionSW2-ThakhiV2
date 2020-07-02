8961371e5879206557f53acf1c74a6452ca4df4f
JhonPeterAguilarAtencio <42424870+JhonPeterAguilarAtencio@users.noreply.github.com>
2020-07-01 21:00:03 -0500

    Creacion de la funcion agregar()
    creacion de la funcion agregar() en el controllers VehiculoController

--- Proyecto-Thakhi-Web-Admin/app/controllers/VehiculoController.php
+++ Proyecto-Thakhi-Web-Admin/app/controllers/VehiculoController.php
@@ -18,6 +18,21 @@ class   VehiculoController{
         require_once _VIEW_PATH_ .'vehiculo/index.php';
         require_once _VIEW_PATH_ . 'footer.php';
     }
+
+    public function agregar() {  
+  
+        $estados = $this->estado->listar();
+
+        if(!empty($_GET['id'])) {
+            $model = $this->vehiculo->obtener($_GET['id']);
+        }
+
+        $nuevo = empty($model->VEHid);
+
+       require_once _VIEW_PATH_ . 'header.php';
+        require_once _VIEW_PATH_ .'vehiculo/agregar.php';
+        require_once _VIEW_PATH_ . 'footer.php';
+    }
 }
 
 ?>
