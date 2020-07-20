b4d5652cf8171687bdd0dde3d9950774ec16f129
J-Alfaro <42424741+J-Alfaro@users.noreply.github.com>
2020-07-20 00:25:43 -0500

    Creacion de constructor de contrlador monitorear

--- Proyecto-Thakhi-Web-Admin/app/controllers/MonitorearController.php
+++ Proyecto-Thakhi-Web-Admin/app/controllers/MonitorearController.php
@@ -1,6 +1,15 @@
 <?php
 namespace App\Controllers;
 
+class   MonitorearController{
+
+    private $monitorear;
+  
+    private $movimientos;
+    public function __construct(){
+        $this->monitorear = new \App\Models\Monitorear;
+      
+    }
 }
 
 ?>
