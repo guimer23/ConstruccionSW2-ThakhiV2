980f852b426761e57983c36314ce12c17e06537c
J-Alfaro <42424741+J-Alfaro@users.noreply.github.com>
2020-07-19 23:58:23 -0500

    creacion funcion listar Vehiclo conductor

--- Proyecto-Thakhi-Web-Admin/app/models/VehiculoConductor.php
+++ Proyecto-Thakhi-Web-Admin/app/models/VehiculoConductor.php
@@ -8,4 +8,24 @@ class VehiculoConductor {
     public function __construct(){
         $this->pdo = \Core\Database::getConnection();
     }
+        public function listar() : array{
+        $result = [];
+
+        try {
+            $sql = "SELECT adm.VECid,con.CONnombre,con.CONapellido,mo.VEHplaca,adm.VECestado from admvectvehiculo_conductor  as adm
+            inner join admcontconductor as con
+           on con.CONdni=adm.CONdni
+           inner join admvehtvehiculo as mo
+           on adm.VEHid= mo.VEHid";
+
+            $stm = $this->pdo->prepare($sql);
+            $stm->execute();
+
+            $result = $stm->fetchAll();
+        } catch(Exception $e) {
+
+        }
+
+        return $result;
+    }
 }
