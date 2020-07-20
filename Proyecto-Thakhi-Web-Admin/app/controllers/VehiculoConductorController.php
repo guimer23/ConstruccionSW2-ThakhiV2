5c34c019a0c12f1e83c34e4703f2c24bf0ee32d8
J-Alfaro <42424741+J-Alfaro@users.noreply.github.com>
2020-07-19 23:52:51 -0500

    Funcion agregar vehiculo conductor

--- Proyecto-Thakhi-Web-Admin/app/controllers/VehiculoConductorController.php
+++ Proyecto-Thakhi-Web-Admin/app/controllers/VehiculoConductorController.php
@@ -18,7 +18,22 @@ class   VehiculoConductorController{
         require_once _VIEW_PATH_ .'vehiculo_conductor/index.php';
         require_once _VIEW_PATH_ . 'footer.php';
     }
-    
+      public function agregar() {  
+      //  $conductores = new \App\Models\Conductor;
+      $conductores=$this->conductor->listar();
+     //   $model2 = new \App\Models\Vehiculo;
+        $vehiculos = $this->vehiculo->listar();
+        if(!empty($_GET['id'])) {
+            $model = $this->vehiculo_conductor->obtener($_GET['id']);
+        }
+
+        $nuevo = empty($model->VECid);
+
+       require_once _VIEW_PATH_ . 'header.php';
+        require_once _VIEW_PATH_ .'vehiculo_conductor/agregar.php';
+        require_once _VIEW_PATH_ . 'footer.php';
+    }
+
 }
 
 ?>
