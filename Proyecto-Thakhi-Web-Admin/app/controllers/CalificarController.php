e0ae9a339356b709591df795b65c5a4f2d5d8780
guimer23 <41280675+guimer23@users.noreply.github.com>
2020-07-20 00:21:52 -0500

    metodo index para controlador calificar

--- Proyecto-Thakhi-Web-Admin/app/controllers/CalificarController.php
+++ Proyecto-Thakhi-Web-Admin/app/controllers/CalificarController.php
@@ -3,6 +3,17 @@
 
     class CalificarController{
 
-       
+        public function __construct(){
+ 
+        }
+
+        public function index() {
+            //$model = $this->usuario->listar();
+            //  $model = $this->empleado->listar();
+            require_once _VIEW_PATH_ . 'header.php';
+            require_once _VIEW_PATH_ .'calificar/index.php';
+            require_once _VIEW_PATH_ . 'footer.php';
+        }
+    
     }
 ?>
