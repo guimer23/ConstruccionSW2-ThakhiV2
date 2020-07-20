a502af55a516062db92b719e55d3cdb91c083189
J-Alfaro <42424741+J-Alfaro@users.noreply.github.com>
2020-07-20 00:26:07 -0500

    Creacion de funcion index

--- Proyecto-Thakhi-Web-Admin/app/controllers/MonitorearController.php
+++ Proyecto-Thakhi-Web-Admin/app/controllers/MonitorearController.php
@@ -10,6 +10,14 @@ class   MonitorearController{
         $this->monitorear = new \App\Models\Monitorear;
       
     }
+       public function index() {
+        $model = $this->monitorear->listar();
+        $movimientos = $this->monitorear->monitorear();
+        require_once _VIEW_PATH_ . 'header.php';
+        require_once _VIEW_PATH_ .'monitorear/index.php';
+        require_once _VIEW_PATH_ . 'footer.php';
+       // require_once _VIEW_PATH_ . 'mapa/marcadores.php';
+    }
 }
 
 ?>
