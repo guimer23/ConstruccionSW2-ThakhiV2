f6c7a704649c7af31a773ae2e1c83dd736440ac1
J-Alfaro <42424741+J-Alfaro@users.noreply.github.com>
2020-07-20 00:26:40 -0500

    Creaacion de funcion monitorea

--- Proyecto-Thakhi-Web-Admin/app/controllers/MonitorearController.php
+++ Proyecto-Thakhi-Web-Admin/app/controllers/MonitorearController.php
@@ -10,12 +10,20 @@ class   MonitorearController{
         $this->monitorear = new \App\Models\Monitorear;
       
     }
-       public function index() {
+   public function index() {
         $model = $this->monitorear->listar();
         $movimientos = $this->monitorear->monitorear();
         require_once _VIEW_PATH_ . 'header.php';
         require_once _VIEW_PATH_ .'monitorear/index.php';
         require_once _VIEW_PATH_ . 'footer.php';
+       // require_once _VIEW_PATH_ . 'mapa/marcadores.php';
+    }
+    public function monitorea() {
+        $movimientos = $this->monitorear->monitorear();
+      
+        //require_once _VIEW_PATH_ . 'header.php';
+        //require_once _VIEW_PATH_ .'monitorear/index.php';
+      //  require_once _VIEW_PATH_ . 'footer.php';
        // require_once _VIEW_PATH_ . 'mapa/marcadores.php';
     }
 }
