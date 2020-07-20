c66d0f6a7d2d1a32dcb693fc8d980f9c39377385
guimer23 <41280675+guimer23@users.noreply.github.com>
2020-07-19 23:52:41 -0500

    Metodo Listar

--- Proyecto-Thakhi-Web-Admin/app/controllers/EntregasController.php
+++ Proyecto-Thakhi-Web-Admin/app/controllers/EntregasController.php
@@ -10,7 +10,28 @@ class   Entregas{
         $this->pdo=\Core\Database::getConnection();
 
     }
+    public function listar() : array{
+        $result = [];
+        try {
+            $sql = "SELECT a.ENTid,a.ENTdescripcion,a.ENTtipo,c.CONnombre ,a.ENTfechahora,cli.CLInombre,a.ENTprecio,a.ENTestado,a.ENTfoto from admenttentrega as a
+            inner join admclitcliente as cli
+            on a.CLIdni=cli.CLIdni
+            inner join admvectvehiculo_conductor as co
+            on a.VECid=co.VECid
+            inner join admcontconductor as c
+            on co.CONdni=c.CONdni
+            order by  a.ENTid desc
+            ";
 
+            $stm = $this->pdo->prepare($sql);
+            $stm->execute();
+
+            $result = $stm->fetchAll();
+        } catch(Exception $e) {
+
+        }
+        return $result;
+    }
 }
 
 ?>
