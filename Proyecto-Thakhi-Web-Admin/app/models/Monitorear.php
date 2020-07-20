40a41bc4124a3fdac07143bb0a13043e505b93c1
J-Alfaro <42424741+J-Alfaro@users.noreply.github.com>
2020-07-20 00:29:18 -0500

    Creacion de funcion listar monitorear

--- Proyecto-Thakhi-Web-Admin/app/models/Monitorear.php
+++ Proyecto-Thakhi-Web-Admin/app/models/Monitorear.php
@@ -9,6 +9,29 @@ class Monitorear{
         $this->pdo=\Core\Database::getConnection();
 
     }
+    public function listar() : array{
+        $result = [];     
+        try {
+            $stm = $this->pdo->prepare('SELECT  vhi.VEHid, vhi.CONdni,co.CONnombre,co.CONapellido,co.CONcelular,vehi.VEHplaca,  vhi.VEClatitud,vhi.VEClongitud,cli.CLInombre,ent.ENTestado FROM admvectvehiculo_conductor as vhi
+            inner join admcontconductor as co
+            on vhi.condni= co.CONdni
+            inner join admvehtvehiculo as vehi
+            on vhi.VEHid=vehi.VEHid
+            inner join admenttentrega as ent
+            on vhi.VECid= ent.VECid
+            inner join admclitcliente as cli
+            on ent.CLIdni =cli.CLIdni
+              where ent.ENTfechahora=curdate()');
+            $stm->execute();
+
+            $result = $stm->fetchAll();
+        } catch(Exception $e) {
+            echo 'ERROR!';
+            print_r( $e );
+        }
+
+        return $result;
+    }
 }
 
 
